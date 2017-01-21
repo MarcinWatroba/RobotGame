5d6737c3cf4f9e0809677d62945a8b4c0da78379
marcin388 <p14170863@my365.dmu.ac.uk>
2017-01-21 23:28:55 +0000

    Added necessary includes to stdafx

--- include/stdafx.h
+++ include/stdafx.h
@@ -10,6 +10,24 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <time.h> 
+
+#include <algorithm>
+#include "gl_core_4_3.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform2.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glfw3.h>
+#include "glslprogram.h"
 
 
 // TODO: reference additional headers your program requires here
